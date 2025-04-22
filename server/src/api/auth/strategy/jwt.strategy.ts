fd2700b4526672deaf41b812e57988bf0be39dd3
Seunggyun Kim <moonrise917@kakao.com>
2025-04-23 02:49:24 +0900

    feat: JWT Strategy 및 Guard 추가

---
+++ server/src/api/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserService } from 'src/api/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('ACCESS_TOKEN_SECRET_KEY') as string,
+    });
+  }
+
+  validate(payload: any) {
+    const { id } = payload;
+    const user = this.userService.findUserByUID(id);
+
+    if (!user) {
+      throw new UnauthorizedException({ message: 'fail - User not found' });
+    }
+
+    return user;
+  }
+}
+
+export interface Payload {
+  id: string;
+}
