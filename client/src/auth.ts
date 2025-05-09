fabc18d8a26144777e4977677c0ae0d541fb603b
Seunggyun Kim <moonrise917@kakao.com>
2025-05-10 04:31:15 +0900

    fix: [Frontend] Backend JWT 토큰 발급에 따른 콜백 함수 수정

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -1,103 +1,107 @@
 import NextAuth from 'next-auth';
-import 'next-auth/jwt';  // Reference: https://github.com/nextauthjs/next-auth/issues/9571
-import Naver from "@auth/core/providers/naver";
+import 'next-auth/jwt'; // Reference: https://github.com/nextauthjs/next-auth/issues/9571
+import Naver from '@auth/core/providers/naver';
 
-export const {
-    handlers,
-    signIn,
-    signOut,
-    auth,
-} = NextAuth({
-    providers: [Naver],
-    session: {
-        strategy: 'jwt'
-    },
-    callbacks: {
-        async signIn({user, account, profile}) {
-            if (account?.provider === 'naver') {
-                try {
-                    const retrievedUserInfo = await backendSignIn({
-                        naverUid: profile?.response?.id as string
-                    });
-                    Object.assign(user, retrievedUserInfo);
-                    return !!profile?.response?.id;
-                } catch (error) {
-                    if (error instanceof Error) {
-                        return '/error';
-                    }
-                }
-            }
-            return true;
-        },
-        jwt({token, user, account, profile, trigger, session}) {
-            if (user) {
-                Object.assign(token, user);
-            }
-            if (account) {
-                token.accessToken = account.access_token;
-                token.id = profile?.response?.id;
-            }
-            if (trigger === 'update') token.name = session.user.name;
-            return token;
-        },
-        async session({session, token}) {
-            session.accessToken = token.accessToken;
-            session.user.id = token.id!;
-            session.user.uid = token.uid as string;
-            return session;
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Naver],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account?.provider === 'naver') {
+        try {
+          const retrievedUserInfo = await backendSignIn({
+            naverUid: profile?.response?.id as string,
+          });
+          Object.assign(user, retrievedUserInfo);
+          return !!profile?.response?.id;
+        } catch (error) {
+          if (error instanceof Error) {
+            return '/error';
+          }
         }
-    }
+      }
+      return true;
+    },
+    jwt({ token, user, account, profile, trigger, session }) {
+      if (user) {
+        Object.assign(token, { user });
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile?.response?.id;
+      }
+      if (trigger === 'update') token.name = session.user.name;
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.user.id = token.user.id!;
+      session.user.uid = token.user.uid as string;
+      return session;
+    },
+  },
 });
 
 async function backendSignIn(body: { naverUid: string }) {
-    const response = await fetch(`${process.env.BACK_API_URL}/auth/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-    });
-    const data = await response.json() as UserResponse | string;
+  const response = await fetch(`${process.env.BACK_API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const data = (await response.json()) as UserResponse | string;
 
-    if (response.ok && typeof data !== 'string') {
-        const {uid, user_info} = data;
+  if (response.ok && typeof data !== 'string') {
+    const { uid, userInfo, accessToken, refreshToken } = data;
 
-        return {
-            uid: uid,
-            channelId: user_info?.channel_id,
-        };
-    }
+    return {
+      uid: uid,
+      channelId: userInfo!.channelId,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    };
+  }
 
-    throw new Error((data || 'Error occurred from server. Try again little bit later.') as string);
+  throw new Error((data || 'Error occurred from server. Try again little bit later.') as string);
 }
 
 declare module 'next-auth' {
-    interface Session {
-        user?: {
-            id: string;
-            uid: string;
-            channelId?: string;
-        }
-        accessToken?: string;
-    }
+  interface Session {
+    user?: {
+      id: string;
+      uid: string;
+      channelId?: string;
+    };
+    accessToken?: string;
+  }
 
-    interface Profile {
-        response?: {
-            id: string;
-        }
-    }
+  interface Profile {
+    response?: {
+      id: string;
+    };
+  }
 }
 
 declare module 'next-auth/jwt' {
-    interface JWT {
-        id?: string;
-        accessToken?: string;
-    }
+  interface JWT {
+    user: {
+      id?: string;
+      uid: string;
+      accessToken?: string;
+      refreshToken?: string;
+    };
+    accessToken?: string;
+  }
 }
 
 interface UserResponse {
-    uid: string;
-    user_info?: {
-        channel_id: string;
-    }
+  uid: string;
+  userInfo?: {
+    channelId: string;
+  };
+  accessToken?: string;
+  refreshToken?: string;
 }
