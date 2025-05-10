3263665b413bfe6488bd5b25dbbadd6ffde31aac
Seunggyun Kim <moonrise917@kakao.com>
2025-05-10 21:49:23 +0900

    fix: [Frontend] Backend에서 발급한 token을 사용하도록 변경

--- client/src/app/api/setting/route.ts
+++ client/src/app/api/setting/route.ts
@@ -1,20 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import * as jwt from 'jsonwebtoken';
 
 export const GET = async (request: NextRequest) => {
   const token = await getToken({ req: request, secret: process.env.AUTH_SECRET as string });
   const response = await fetch(`${process.env.BACK_API_URL}/setting`, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${jwt.sign(
-        JSON.stringify({
-          user: {
-            id: token?.id,
-          },
-        }),
-        process.env.AUTH_SECRET as string
-      )}`,
+      Authorization: `Bearer ${token?.user.accessToken}`,
     },
   });
 
