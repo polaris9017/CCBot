a51dc92866f2050d3574ead6bb4ae89ecb46d0b8
Seunggyun Kim <moonrise917@kakao.com>
2025-05-23 03:37:18 +0900

    feat: [Frontend] 에러 페이지 공통 요소 분리

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,30 +1,28 @@
 /* Layout from Tailwind UI (https://tailwindcss.com/plus/ui-blocks/marketing/feedback/404-pages) */
 
 import Link from 'next/link';
+import { ReactNode } from 'react';
+import ErrorPageContainer from '@/components/ErrorPageContainer';
 
 export default function NotFound() {
+  const links: ReactNode[] = [
+    <Link
+      href="/"
+      className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      key="return-home"
+    >
+      홈으로 돌아가기
+    </Link>,
+    <a href="#" className="text-sm font-semibold text-gray-900" key="support">
+      Contact support <span aria-hidden="true">&rarr;</span>
+    </a>,
+  ];
+
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-          Page not found
-        </h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-          해당 페이지를 찾을 수 없습니다.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            홈으로 돌아가기
-          </Link>
-          <a href="#" className="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
-        </div>
-      </div>
-    </main>
+    <ErrorPageContainer
+      title="Page not found"
+      message="해당 페이지를 찾을 수 없습니다."
+      linkList={links}
+    />
   );
 }
