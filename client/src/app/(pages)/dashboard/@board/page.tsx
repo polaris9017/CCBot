21d4ecb7ec5bd03c7fd33eefcada705651c08e4c
Seunggyun Kim <moonrise917@kakao.com>
2025-05-26 03:52:27 +0900

    feat: [Frontend] 대시보드 메인 페이지 기본 구조 작성

--- client/src/app/(pages)/dashboard/@board/page.tsx
+++ client/src/app/(pages)/dashboard/@board/page.tsx
@@ -7,7 +7,24 @@ async function Board() {
   const uid = session?.user.uid;
   const channelId = session?.user.channelId;
 
-  return <>{channelId ? channelId : <BoardPageComponent uid={uid!} />}</>;
+  return (
+    <>
+      {channelId ? (
+        <>
+          <main>
+            <div className="flex items-center justify-center p-4">
+              <h1 className="text-4xl font-bold text-center">안녕하세요!</h1>
+            </div>
+            <div className="flex items-center justify-center p-4">
+              <h4 className="text-center">좌상단 메뉴에서 항목을 선택하세요</h4>
+            </div>
+          </main>
+        </>
+      ) : (
+        <BoardPageComponent uid={uid!} />
+      )}
+    </>
+  );
 }
 
 export default Board;
