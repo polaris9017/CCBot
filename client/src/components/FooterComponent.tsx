aa79931acba0a11585d6d3110f95074a7d9fab5b
Seunggyun Kim <moonrise917@kakao.com>
2025-06-02 18:29:14 +0900

    feat: [Frontend] Footer 컴포넌트 추가 및 기재 사항 추가

--- client/src/components/FooterComponent.tsx
+++ client/src/components/FooterComponent.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import Link from 'next/link';
 
 export default function FooterComponent() {
+  const startYear: number = 2025;
+  const currentYear = useMemo(() => new Date().getFullYear(), []);
+  const copyrightSubject = 'CCBot by polaris9017';
+
   return (
-    <footer className="sticky bottom-0 w-full bg-gray-200 border-t border-gray-200 z-50">
+    <footer className="sticky bottom-0 flex flex-row justify-between w-full bg-gray-200 border-t border-gray-200 z-50">
       <div className="max-w-7xl mx-auto px-4 py-6">
         <p className="text-center text-gray-600 text-sm">
-          © {new Date().getFullYear()} Your Company. All rights reserved.
+          &copy; {startYear} {startYear === currentYear ? '' : `- ${currentYear}`}{' '}
+          {copyrightSubject}. All rights reserved.
+        </p>
+      </div>
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <p className="text-center text-gray-600 text-sm">
+          <Link href="#">개인정보처리방침</Link>
+          {'  |  '}
+          <Link href="#">이용약관</Link>
         </p>
       </div>
     </footer>
